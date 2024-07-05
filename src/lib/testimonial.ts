97edfcb41752102a9eaa1d6561234a7f67d0e48f
gayatri fatale <fatalegayatri24@gmail.com>
2024-07-05 13:15:05 +0530

    chore: Update npm dependency to latest stable version

---
+++ src/lib/testimonial.ts
@@ -0,0 +1,40 @@
+export const Testimonials = [
+    {
+        id: 1,
+        content: "“Lisa and her team were passionate and highly engaged in my mission and ​visibility. I appreciated their commitment to excellence and learned so much ​about effective messaging and delivery platforms as well. I highly ​recommend any entrepreneur or organization have a strategy session with ​TMM to reach their goals.”",
+        name: "Kathi McCarty",
+        post: "Founder/Director, Meth Toxins Awareness Alliance ",
+        img: 'assets/casper-assets/testimonials/avatar-4.png',
+
+    },
+    {
+        id: 2,
+        content: "Fox Valley Mutual Inc. utilized Lisa Carter and her team for a complete ​company overhaul when I increased my company presence in 7 states. They ​single handedly recreated my logo, my fonts, my email signature, my ​business card and brochure as well as my website and its presence with SEO. ​If you are in need of a one stop shop and attention to detail, all for a fair price, ​look no further! \n Thank you, Lisa and Robin, for such care to my business! I highly recommend ​your services!",
+        name: "Lisa Marie Schwartz",
+        post: " Founder/Owner/Broker, Fox Valley Mutual Inc.",
+        img: 'assets/casper-assets/testimonials/avatar-2.png',
+
+    },
+    {
+        id: 3,
+        content: "Triple Motive Marketing has created a new name, new logo, business card, ​brochure, flyer, presentation and built a new website, working patiently at ​my speed. They improved the functionality with a better template, ​meaningful content, inviting shopping pages, call to action and video ​coaching and editing. My website is easier to use, is more professional and I ​am elated with my website and the feedback I am getting. Thanks Triple ​Motive Marketing!!",
+        name: "Vera Shury ",
+        post: "Owner, Vera Shury Art Studio",
+        img: 'assets/casper-assets/testimonials/avatar-3.png',
+    },
+    {
+        id: 4,
+        content: "“Lisa went above and beyond the call, immersing herself in who we are and ​what we do as an organization in order to help us find the right notes and ​hone our pitch.”",
+        name: "David P ",
+        post: "Advocacy and Communications, Winter Wildlands Alliance",
+        img: "assets/casper-assets/testimonials/avatar-4.png"
+    },
+    {
+        id: 5,
+        content: "“Lisa was extremely thoughtful and thorough in her approach. She has a ​wealth of understanding regarding the creation of foundational statements ​for organizations.”",
+        name: "Phil S.",
+        post: " Executive Director, Bike Utah",
+        img: "assets/casper-assets/testimonials/avatar-1.png"
+    },
+
+]
